e7aeedf3b17484f3bc7c12e6f44d5092ee5fe5bc
Tinius <tiniusw@uio.no>
2025-08-02 18:28:33 +0200

    Sentiment analysis with csv output

--- SentimentAnalysis/Customer_reviews_sentiment.py
+++ SentimentAnalysis/Customer_reviews_sentiment.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import pyodbc # Pyodbc is a Python library that allows you to connect to Microsoft SQL Server databases using ODBC.
 import nltk
-from nltk.sentimen.vader import SentimentIntensityAnalyzer
-
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import Customer_reviews_sentiment
 # Download the Vader lexicon for sentiment analysis
 nltk.download('vader_lexicon')
-
+#print(pyodbc.drivers())
+print(Customer_reviews_sentiment)
+'''
 def fetch_data_sql():
     connenction_str = (
-        "Driver={SQL SERVER};"
-        "Server=TINIUS;"
-        "Database=PortfolioProject_MarketingAnalystics;"
-        "Trusted_Connection = yes;"
+        #Establish the string for connecting to my databse
+        "Driver={ODBC Driver 17 for SQL Server};"  
+         #Redacted as database is deleted after this csv pull, will add .bak file to project
+        "Server=XX" 
+        "Database=XX;"  
+        "Trusted_Connection=yes;"
     )
     #Establish connection to the SQL database
     conn = pyodbc.connect(connenction_str)
@@ -48,5 +52,31 @@ def categorize_sentiment(score,rating):
         else:
             return "Mixed Positive" #Negative sentiment and high rating
     else:
-        return "Neutral" #Neutral sentiment and medium rating
-        
+        if rating > 4:
+            return "Positive" # Postivite rating and neutral sentiment
+        elif rating <= 2:
+            return "Negative" #Negative rating and netural sentiment score
+        else:
+            return "Neutral" #Neutral rating and sentiment
+
+#Bucket to score sentiment into ranges 
+def sentiment_buckets(score):
+    if score >= 0.5:
+        return "0.5 to 1.0"
+    elif 0.0   <= score <0.5:
+        return "0.0 to 0.49"
+    elif -0.5 <= score <0.0:
+        return "-0.5 to 0.0"
+    else:
+        return "-1.0 to -0.5" 
+
+#Apply sentiment analysis to calculate the sentiment scores for each review, categorization and apply bucketing to defined ranges 
+customer_reviews_df['SentimentScore'] = customer_reviews_df['ReviewText'].apply(calculate_sentiment)
+customer_reviews_df['SentimentCategory'] = customer_reviews_df.apply(
+    lambda row: categorize_sentiment(row['SentimentScore'],row['Rating']), axis = 1)
+     
+customer_reviews_df['SentimentBucket'] = customer_reviews_df['SentimentScore'].apply(sentiment_buckets)
+
+print(customer_reviews_df.head())
+#Save the dataframe to a .csv file
+customer_reviews_df.to_csv('customer_review_with_sentiment.csv',index = False) '''

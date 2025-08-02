9ddd125638d134a94a845b3ed2c68dd656e5f7f2
Tinius <tiniusw@uio.no>
2025-08-02 17:58:20 +0200

    Sentiment analysis on modified sql data

--- SentimentAnalysis/Customer_reviews_sentiment.py
+++ SentimentAnalysis/Customer_reviews_sentiment.py
@@ -7,4 +7,46 @@ from nltk.sentimen.vader import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
 
 def fetch_data_sql():
-    
+    connenction_str = (
+        "Driver={SQL SERVER};"
+        "Server=TINIUS;"
+        "Database=PortfolioProject_MarketingAnalystics;"
+        "Trusted_Connection = yes;"
+    )
+    #Establish connection to the SQL database
+    conn = pyodbc.connect(connenction_str)
+    #Fetch data from the Cstomer review table
+    query = "SELECT ReviewID,CustomerID,ProductID,ReviewDate,Rating,ReviewText FROM customer_review"
+    df = pd.read_sql(query, conn)
+    conn.close()
+    #Returning data from the database
+    return df
+
+customer_reviews_df = fetch_data_sql()
+
+#Initalize the Vader Sentiment Intensity Analyzer
+sia = SentimentIntensityAnalyzer()
+
+def calculate_sentiment(review_text):
+    sentiment = sia.polarity_scores(review_text) 
+    return sentiment['compound']
+
+def categorize_sentiment(score,rating):
+    #Combining the sentiment score and numeric rating give to determine the sentiment category
+    if score >= 0.05:
+        if rating >= 4:
+            return "Positive" #Positive sentiment and high rating
+        elif rating == 3:
+            return "Mixed Positive" # Positive sentiment and medium rating
+        else:
+            return "Mixed Negative" #Positive sentiment and low rating
+    elif score <= -0.05:
+        if rating <= 2:
+            return "Negative" #Negative sentiment and low rating
+        elif rating == 3:
+            return "Mixed Negative" #Negative sentiment and medium rating
+        else:
+            return "Mixed Positive" #Negative sentiment and high rating
+    else:
+        return "Neutral" #Neutral sentiment and medium rating
+        
